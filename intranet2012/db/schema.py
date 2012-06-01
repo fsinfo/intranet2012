ab06df54379c8874234fe5bfe4f3bd51c5c6a4d2
Julius Roob <julius@juliusroob.de>
2012-06-01 16:18:59 +0200

    Second pard of first draft of first db schema.

--- intranet2012/db/schema.py
+++ intranet2012/db/schema.py
@@ -4,7 +4,7 @@ schema = MetaData()
 
 addressbook = Table('addressbook', schema,
                     Column('address_id', Integer, primary_key = True),
-                    Column('username', String(16), nullable = True),
+                    Column('ldap_username', String(32), nullable = True),
                     Column('firstname', String(32), nullable = True),
                     Column('lastname', String(32), nullable = True),
                     Column('nickname', String(32), nullable = True),
@@ -31,5 +31,41 @@ addressbook_tags = Table('addressbook_tags', schema,
 addressbook_tag_relation = Table('addressbook_tag_relation', schema,
                                  Column('address_id', Integer, ForeignKey('addressbook.address_id'), nullable = False),
                                  Column('tag_id', Integer, ForeignKey('addressbook_tags.tag_id'), nullable = False))
+
+beverage_category = Table('beverage_category', schema,
+                          Column('category_id', Integer, primary_key = True),
+                          Column('name', String(32)))
+
+beverage_item = Table('beverage_item', schema,
+                      Column('item_id', Integer, primary_key = True),
+                      Column('category_id', Integer, ForeignKey('beverage_category.category_id'), nullable = True))
+                      Column('name', String(32), nullable = False),
+                      Column('unit', String(32), nullable = True)) # Amount of said beverage to be sold per price… … penis
+
+beverage_prices_list = Table('beverage_prices_list', schema,
+                             Column('list_id', Integer, primary_key = True),
+                             Column('from', Date(), nullable = False),
+                             Column('to', Date(), nullable = False))
+
+beverage_prices_item = Table('beverage_prices_item', schema,
+                             Column('item_id', Integer, ForeignKey('beverage_item.item_id'), nullable = False),
+                             Column('list_id', Integer, ForeignKey('beverage_prices_list.list_id'), nullable = False),
+                             Column('internal_price', Decimal(6, 2), nullable=False),
+                             Column('external_price', Decimal(6, 2), nullable=False))
+
+beverage_tap = Table('beverage_tap', schema,
+                     Column('tap_id', Integer, primary_key = True),
+                     Column('list_id', Integer, ForeignKey('beverage_prices_list.list_id'), nullable = False),
+                     Column('from', Date(), nullable = False),
+                     Column('to', Date(), nullable = False))
+
+beverage_customer = Table('beverage_customer', schema,
+                          Column('customer_id', Integer, primary_key = True),
+                          Column('ldap_username', String(32)))
+
+beverage_customer_taps = Table('beverage_customer_taps', schema,
+                               Column('customer_id', Integer, ForeignKey('beverage_customer.customer_id'), nullable = False),
+                               Column('tap_id', Integer, ForeignKey('beverage_tap.tap_id'), nullable = False))
+
 
 
